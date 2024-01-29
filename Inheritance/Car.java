7b8c23e3001593ab2ace1642b85a92268d5e160e
thisisaayush <thakurikarthik@gmail.com>
2024-01-28 18:48:03 -0600

    - practiced encapsulation coding problems.

---
+++ Inheritance/Car.java
@@ -0,0 +1,67 @@
+package Inheritance;
+
+public class Car {
+    //Encapsulation Integration.
+    private String company = "Tesla";
+    private String model = "Model X";
+    private String color = "Gray";
+    private int doors = 2;
+    private boolean convertible = true;
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void setCompany(String company) {
+
+        if (company == null) company = "Unknown";
+        String lowercaseMake = company.toLowerCase();
+        switch (lowercaseMake) {
+            case "holden", "porsche", "tesla", "porsche targa" -> this.company = company;
+            default -> {
+                this.company = "Unsupported ";
+            }
+        }
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
+    public void describeCar() {
+
+        System.out.println(doors + "-Door " +
+                color + " " +
+                company + " " +
+                model + " " +
+                (convertible ? "Convertible" : ""));
+    }
+}
---
+++ Inheritance/Main.java
@@ -0,0 +1,32 @@
+package Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.setCompany("Porsche");
+        car.setModel("Carrera");
+        car.setDoors(2);
+        car.setConvertible(true);
+        car.setColor("black");
+        System.out.println("Company = " + car.getCompany());
+        System.out.println("model = " + car.getModel());
+        car.describeCar();
+
+        Car targa = new Car();
+        targa.setCompany("Porsche Targa");
+        targa.setModel("Targa");
+        targa.setDoors(2);
+        targa.setConvertible(false);
+        targa.setColor("red");
+        targa.describeCar();
+
+        Car newProduct = new Car();
+        newProduct.setCompany("Tesla");
+        newProduct.setModel("Model XX");
+        newProduct.setColor("Gray Light");
+        newProduct.setDoors(1);
+        newProduct.describeCar();
+
+    }
+}
